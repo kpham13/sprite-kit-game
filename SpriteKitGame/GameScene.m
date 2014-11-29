a3036b2cb790e97e8dd2e7a1261784e95907a258
Kevin Pham <kevin.v.pham@hotmail.com>
2014-11-29 01:44:32 -0800

    added spaceship and scrolling background

--- SpriteKitGame/GameScene.m
+++ SpriteKitGame/GameScene.m
@@ -8,42 +8,150 @@
 
 #import "GameScene.h"
 
-@implementation GameScene
+static const uint32_t shipCategory =  0x1 << 0;
+static const uint32_t obstacleCategory =  0x1 << 1;
 
--(void)didMoveToView:(SKView *)view {
-    /* Setup your scene here */
-    SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
-    
-    myLabel.text = @"Hello, World!";
-    myLabel.fontSize = 65;
-    myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                   CGRectGetMidY(self.frame));
-    
-    [self addChild:myLabel];
+static const float BG_VELOCITY = 100.0; // Velocity with which our background is going to move
+static inline CGPoint CGPointAdd(const CGPoint a, const CGPoint b) {
+    return CGPointMake(a.x + b.x, a.y + b.y);
+}
+static inline CGPoint CGPointMultiplyScalar(const CGPoint a, const CGFloat b) {
+    return CGPointMake(a.x * b, a.y * b);
 }
 
--(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-    /* Called when a touch begins */
+
+
+@implementation GameScene {
+    SKSpriteNode *ship;
+    SKAction *actionMoveUp;
+    SKAction *actionMoveDown;
+    NSTimeInterval _lastUpdateTime;
+    NSTimeInterval _dt;
+    NSTimeInterval _lastMissileAdded;
+}
+
+-(id)initWithSize:(CGSize)size {
+    if (self = [super initWithSize:size]) {
+        self.backgroundColor = [SKColor whiteColor];
+        [self initalizingScrollingBackground];
+        [self addShip];
+        
+        // Making self delegate of physics World
+        self.physicsWorld.gravity = CGVectorMake(0,0);
+        self.physicsWorld.contactDelegate = self;
+    }
     
-    for (UITouch *touch in touches) {
-        CGPoint location = [touch locationInNode:self];
-        
-        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-        
-        sprite.xScale = 0.5;
-        sprite.yScale = 0.5;
-        sprite.position = location;
-        
-        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-        
-        [sprite runAction:[SKAction repeatActionForever:action]];
-        
-        [self addChild:sprite];
+    return self;
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    UITouch *touch = [touches anyObject];
+    CGPoint touchLocation = [touch locationInNode:self.scene];
+    if(touchLocation.y >ship.position.y){
+        if(ship.position.y < 300){
+            [ship runAction:actionMoveUp];
+        }
+    }else{
+        if(ship.position.y > 50){
+            [ship runAction:actionMoveDown];
+        }
     }
 }
 
--(void)update:(CFTimeInterval)currentTime {
-    /* Called before each frame is rendered */
+-(void)addShip {
+    // Initializing spaceship node
+    ship = [SKSpriteNode spriteNodeWithImageNamed:@"spaceship"];
+    [ship setScale:0.5];
+    ship.zRotation = - M_PI / 2;
+    
+    // Adding Sprite Kit physics body for collision detection
+    ship.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:ship.size];
+    ship.physicsBody.categoryBitMask = shipCategory;
+    ship.physicsBody.dynamic = YES;
+    ship.physicsBody.contactTestBitMask = obstacleCategory;
+    ship.physicsBody.collisionBitMask = 0;
+    ship.name = @"ship";
+    ship.position = CGPointMake(120,160);
+    
+    [self addChild:ship];
+    
+    actionMoveUp = [SKAction moveByX:0 y:30 duration:.2];
+    actionMoveDown = [SKAction moveByX:0 y:-30 duration:.2];
 }
+
+-(void)initalizingScrollingBackground {
+    for (int i = 0; i < 2; i++) {
+        SKSpriteNode *bg = [SKSpriteNode spriteNodeWithImageNamed:@"bg"];
+        bg.position = CGPointMake(i * bg.size.width, 0);
+        bg.anchorPoint = CGPointZero;
+        bg.name = @"bg";
+        [self addChild:bg];
+    }
+    
+}
+
+- (void)moveBg {
+    [self enumerateChildNodesWithName:@"bg" usingBlock: ^(SKNode *node, BOOL *stop) {
+         SKSpriteNode * bg = (SKSpriteNode *) node;
+         CGPoint bgVelocity = CGPointMake(-BG_VELOCITY, 0);
+         CGPoint amtToMove = CGPointMultiplyScalar(bgVelocity,_dt);
+         bg.position = CGPointAdd(bg.position, amtToMove);
+         
+         // Checks if bg node is completely scrolled of the screen, if yes then put it at the end of the other node
+         if (bg.position.x <= -bg.size.width) {
+             bg.position = CGPointMake(bg.position.x + bg.size.width*2, bg.position.y);
+         }
+     }];
+}
+
+-(void)update:(CFTimeInterval)currentTime {
+    if (_lastUpdateTime)
+    {
+        _dt = currentTime - _lastUpdateTime;
+    }
+    else
+    {
+        _dt = 0;
+    }
+    _lastUpdateTime = currentTime;
+    
+    [self moveBg];
+}
+
+//-(void)didMoveToView:(SKView *)view {
+//    /* Setup your scene here */
+//    SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
+//    
+//    myLabel.text = @"Hello, World!";
+//    myLabel.fontSize = 65;
+//    myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
+//                                   CGRectGetMidY(self.frame));
+//    
+//    [self addChild:myLabel];
+//}
+//
+//-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+//    /* Called when a touch begins */
+//    
+//    for (UITouch *touch in touches) {
+//        CGPoint location = [touch locationInNode:self];
+//        
+//        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
+//        
+//        sprite.xScale = 0.5;
+//        sprite.yScale = 0.5;
+//        sprite.position = location;
+//        
+//        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
+//        
+//        [sprite runAction:[SKAction repeatActionForever:action]];
+//        
+//        [self addChild:sprite];
+//    }
+//}
+//
+//-(void)update:(CFTimeInterval)currentTime {
+//    /* Called before each frame is rendered */
+//}
 
 @end
