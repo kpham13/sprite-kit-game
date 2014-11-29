462102772d2c111cca4c24c54ac2e431196559a3
Kevin Pham <kevin.v.pham@hotmail.com>
2014-11-29 01:04:06 -0800

    initial commit

---
+++ SpriteKitGame/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  SpriteKitGame
+//
+
+//  Copyright (c) 2014 Kevin Pham. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ SpriteKitGameTests/SpriteKitGameTests.m
@@ -0,0 +1,40 @@
+//
+//  SpriteKitGameTests.m
+//  SpriteKitGameTests
+//
+//  Created by Kevin Pham on 11/29/14.
+//  Copyright (c) 2014 Kevin Pham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface SpriteKitGameTests : XCTestCase
+
+@end
+
+@implementation SpriteKitGameTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
